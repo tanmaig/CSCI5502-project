985cc4445137afcdb5a23c6a7d450d1440f80672
SowjanyaAchar <soujanya.achar@gmail.com>
2020-11-10 17:22:56 -0700

    added Label generation

--- main.py
+++ main.py
@@ -20,6 +20,28 @@ def data_cleaning(region_wise_datasets):
     return region_wise_datasets
 
 
+def label_generation(dataset):
+    """
+    Descripton: Function to generate labels for consolidated dataset using score function. Labels are defined as follows:
+    1. class 0: Non popular videos; (< 100000 views)
+    2. class 1: Popular videos with overwhelming bad views. (>= 100000 views, score < 0)
+    3. class 2: Neutral popular videos ( >= 100000 views, score >= 0 & score < 300)
+    4. class 3: Popular videos with overwhleming good views. (>= 100000, score >= 300)
+
+    Score function is calculated as follows:
+    score = (likes - 1.5*dislikes)*(comment_count / views)
+
+    :param dataset: Dataframe containing condolidated region wise dataset.
+    :return: Labels (pd.Series)
+    """
+    labels = dataset.apply(lambda x: (x["likes"] - 1.5*x["dislikes"])*(x["comment_count"]/x["views"]), axis=1)
+    labels = labels.apply(lambda x: 1 if x < 0 else (2 if x >= 0 and x < 300 else 3))
+    return labels
+
+
+def feature_generation():
+
+
 if __name__ == '__main__':
 
     # Load command line arguments
@@ -57,3 +79,17 @@ if __name__ == '__main__':
     # Step 2: Save dictionary containing region wise cleaned datasets. => This can be used separately for exploratory
     # data analysis.
     write_pickle_file(region_wise_datasets, output_path + "/cleaned_data.pkl")
+
+
+    # Step 3: Consolidating US and Canada data sets into a single data set
+    dataset = pd.concat([region_wise_datasets['US'],region_wise_datasets['CA']], ignore_index=True)
+
+    # Deleting the Dictionary because it's no longer in use.
+    del region_wise_datasets
+
+    # Generate labels using score function.
+    dataset["label"] = label_generation(dataset)
+
+    # Feature generation: Generate base features and derived features.
+
+    print(dataset)
