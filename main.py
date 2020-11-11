7d150e813d811bb3e42757a1c41ae7ba92e951f6
Your Name <you@example.com>
2020-11-11 03:15:56 -0700

    added feature generation

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import argparse
+import numpy as np
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
 from util import write_pickle_file, exists, determine_regions, read_datasets
 
 
@@ -39,7 +46,126 @@ def label_generation(dataset):
     return labels
 
 
-def feature_generation():
+def load_glove_vectors(glove_vector_path):
+    """
+    Description: Function to create a dictionary mapping words to glove vector embeddings.
+    :param glove_vector_path: Path to file containing 25d pretrained glove vectors extracted from Twitter data.
+    :return: Dictionary with reference glove vector embeddings.
+    """
+    embeddings_dict = {}
+    with open(glove_vector_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        elements = line.split()
+        word = elements[0]
+        embedding = np.asarray(elements[1:], "float64")
+        embeddings_dict[word] = embedding
+    return embeddings_dict
+
+
+def text_preprocessing(data):
+    """
+    Description: Function to preprocess all the NLP features.
+    :param data: Row of Dataframe.
+    :return: preprocessed row.
+    """
+    # normalize data
+    data["title"] = str(data["title"]).lower()
+    data["tags"] = str(data["tags"]).lower()
+    data["description"] = str(data["description"]).lower()
+
+    # remove non alphanumeric characters
+    data["title"] = re.sub("[^a-zA-Z0-9]", " ", data["title"])
+    data["tags"] = re.sub("[^a-zA-Z0-9]", " ", data["tags"])
+    data["description"] = re.sub("[^a-zA-Z0-9]", " ", data["description"])
+
+    # tokenization
+    data["title"] = word_tokenize(data["title"])
+    data["tags"] = word_tokenize(data["tags"])
+    data["description"] = word_tokenize(data["description"])
+
+    # Remove stopwords
+    stop_words = set(stopwords.words("english"))
+    data["title"] = [token for token in data["title"] if token not in stop_words]
+    data["tags"] = [token for token in data["tags"] if token not in stop_words]
+    data["description"] = [token for token in data["description"] if token not in stop_words]
+
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer() # intialize WordNet lemmagtizer
+    #data["title"] = [lemmatizer.lemmatize(w, tag) for (w, tag) in nltk.pos_tag(data["title"])]
+    #data["tags"] = [lemmatizer.lemmatize(w, tag) for (w, tag) in nltk.pos_tag(data["tags"])]
+    #data["description"] = [lemmatizer.lemmatize(w, tag) for (w, tag) in nltk.pos_tag(data["description"])]
+    data["title"] = [lemmatizer.lemmatize(w) for w in data["title"]]
+    data["tags"] = [lemmatizer.lemmatize(w) for w in data["tags"]]
+    data["description"] = [lemmatizer.lemmatize(w) for w in data["description"]]
+
+    # remove words with len <= 2
+    data["title"] = [word for word in data["title"] if len(word) > 2]
+    data["tags"] = [word for word in data["tags"] if len(word) > 2]
+    data["description"] = [word for word in data["description"] if len(word) > 2]
+
+    return data
+
+
+def embedding(data, embeddings_dict):
+    """
+    Description: Convert preprocessed textual data into vector form using glove vectors. For a sentence, all word
+    embeddings are aggregated.
+    :param data: Row of input dataframe.
+    :param embeddings_dict: Dictionary containing word to embedding mapping.
+    :return: Row with textual data transformed using glove vector embeddings.
+    """
+
+    temp = np.zeros(25)
+    for elem in data["title"]:
+        try:
+            temp = np.sum([temp, embeddings_dict[elem]], axis=0)
+        except KeyError:
+            pass
+    data["title"] = temp
+
+    temp = np.zeros(25)
+    for elem in data["tags"]:
+        try:
+            temp = np.sum([temp, embeddings_dict[elem]], axis=0)
+        except KeyError:
+            pass
+    data["tags"] = temp
+
+    temp = np.zeros(25)
+    for elem in data["description"]:
+        try:
+            temp = np.sum([temp, embeddings_dict[elem]], axis=0)
+        except KeyError:
+            pass
+    data["description"] = temp
+    return data
+
+
+def feature_generation(dataset, embeddings_dict):
+    """
+    Description: Function to transform dataset columns to generate base / derived features.
+    :param dataset: Dataframe containing input dataset.
+    :param embeddings_dict: Dictionary containing word to embedding mapping.
+    :return: Dataset (Dataframe) containing generated features.
+    """
+    # Feature 1: time_gap = No. of days between publishing date and trending date.
+    trending_date = pd.to_datetime(dataset["trending_date"].head(), format="%y.%d.%m").dt.date
+    publish_date = pd.to_datetime(dataset["publish_time"].head()).dt.date
+    dataset["time_gap"] = (trending_date - publish_date).dt.days
+
+    # Feature 2, 3, 4: title, tags, description - NLP features
+    dataset = dataset.apply(text_preprocessing, axis=1)
+    dataset = dataset.apply(embedding, args=(embeddings_dict,), axis=1)
+
+    # Feature 5: category_id : Same categories used in all regions (labelled from 1 to 44: 3 to 10 missing). (Should intercept be added?)
+    # Feature 6: duration : This will be considered, if present.
+
+    features_list = ["video_id", "time_gap", "title", "tags", "description", "category_id"]
+    if "duration" in dataset.columns:
+        features_list.append("duration")
+    dataset = dataset[features_list]
+    return dataset
 
 
 if __name__ == '__main__':
@@ -57,12 +183,17 @@ if __name__ == '__main__':
                         default="./output",
                         type=lambda x: exists(x, "dir"),
                         help="Path to output folder.")
+    parser.add_argument("--glove_vector_path",
+                        default="./glove.twitter.27B.25d.txt",
+                        type=lambda x: exists(x, "file"),
+                        help="Path to file containing pre-trained glove vectors.")
 
     args = parser.parse_args()
 
     dataset_path = args.dataset_path
     regions = args.regions
     output_path = args.output_path
+    glove_vector_path = args.glove_vector_path
 
     #print(dataset_path)
     #print(regions)
@@ -91,5 +222,10 @@ if __name__ == '__main__':
     dataset["label"] = label_generation(dataset)
 
     # Feature generation: Generate base features and derived features.
+    # For the language features, we need to transform them into features of numerical form
+    # (this is done using word embeddings: GloVe vectors)
+    embeddings_dict = load_glove_vectors(glove_vector_path) # Dictionary containing word, embedding pair obtained from reference file.
+    dataset = feature_generation(dataset, embeddings_dict) # transform columns and generate features.
 
-    print(dataset)
+    # Going forward, ndarray would be suitable for input to most models instead of dataframe, I think.
+    print(dataset.head())
